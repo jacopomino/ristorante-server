cfd6eb920915d4a642c961a97b6eb926b8eddfc4
jacopomino <92917960+jacopomino@users.noreply.github.com>
2023-04-01 10:19:25 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.get("/ristoranti", async (req,res)=>{
 //put signup
 app.put("/signup", async (req,res)=>{
     let info=JSON.parse(Object.keys(req.body)[0]);
-    let client=new MongoClient("mongodb+srv://apo:jac2001min@cluster0.pdunp.mongodb.net/?retryWrites=true&w=majority")
+    let client=new MongoClient("mongodb://apo:jac2001min@cluster0-shard-00-00.pdunp.mongodb.net:27017,cluster0-shard-00-01.pdunp.mongodb.net:27017,cluster0-shard-00-02.pdunp.mongodb.net:27017/?ssl=true&replicaSet=atlas-me2tz8-shard-0&authSource=admin&retryWrites=true&w=majority")
     client.db("ristoro").collection("ristoranti").insertOne(info,(err,result)=>{
         if (err) throw err;
     })
@@ -26,7 +26,7 @@ app.put("/signup", async (req,res)=>{
 //add elemento in dispensa
 app.put("/add-dispensa", async (req,res)=>{
     let info=JSON.parse(Object.keys(req.body)[0]);
-    let client=new MongoClient("mongodb+srv://apo:jac2001min@cluster0.pdunp.mongodb.net/?retryWrites=true&w=majority")
+    let client=new MongoClient("mongodb://apo:jac2001min@cluster0-shard-00-00.pdunp.mongodb.net:27017,cluster0-shard-00-01.pdunp.mongodb.net:27017,cluster0-shard-00-02.pdunp.mongodb.net:27017/?ssl=true&replicaSet=atlas-me2tz8-shard-0&authSource=admin&retryWrites=true&w=majority")
     client.db("ristoro").collection("ristoranti").updateOne({_id:new ObjectId(info.id)},{$push:{"dispensa":{id:new ObjectId(),elemento:info.elemento,categoria:info.categoria}}},(err,result)=>{
         if (err) throw err;
     })
@@ -34,7 +34,7 @@ app.put("/add-dispensa", async (req,res)=>{
 //modify elemento in dispensa
 app.put("/modify-dispensa", async (req,res)=>{
     let info=JSON.parse(Object.keys(req.body)[0]);
-    let client=new MongoClient("mongodb+srv://apo:jac2001min@cluster0.pdunp.mongodb.net/?retryWrites=true&w=majority")
+    let client=new MongoClient("mongodb://apo:jac2001min@cluster0-shard-00-00.pdunp.mongodb.net:27017,cluster0-shard-00-01.pdunp.mongodb.net:27017,cluster0-shard-00-02.pdunp.mongodb.net:27017/?ssl=true&replicaSet=atlas-me2tz8-shard-0&authSource=admin&retryWrites=true&w=majority")
     client.db("ristoro").collection("ristoranti").updateOne({_id:new ObjectId(info.id),"dispensa.id":new ObjectId(info.idDispensa)},{$set:{"dispensa.$.quantita":info.quantita}},(err,result)=>{
         if (err) throw err;
     })
@@ -42,15 +42,15 @@ app.put("/modify-dispensa", async (req,res)=>{
 //add cart
 app.put("/add-cart", async (req,res)=>{
     let info=JSON.parse(Object.keys(req.body)[0]);
-    let client=new MongoClient("mongodb+srv://apo:jac2001min@cluster0.pdunp.mongodb.net/?retryWrites=true&w=majority")
-    client.db("ristoro").collection("ristoranti").updateOne({_id:new ObjectId(info.id)},{$push:{carrello:{id:info.idCarrello,elemento:info.elemento}}},(err,result)=>{
+    let client=new MongoClient("mongodb://apo:jac2001min@cluster0-shard-00-00.pdunp.mongodb.net:27017,cluster0-shard-00-01.pdunp.mongodb.net:27017,cluster0-shard-00-02.pdunp.mongodb.net:27017/?ssl=true&replicaSet=atlas-me2tz8-shard-0&authSource=admin&retryWrites=true&w=majority")
+    client.db("ristoro").collection("ristoranti").updateOne({_id:new ObjectId(info.id)},{$push:{carrello:{id:new ObjectId(info.idCarrello),elemento:info.elemento}}},(err,result)=>{
         if (err) throw err;
     })
 })
 //modify elemento in cart
 app.put("/modify-carrello", async (req,res)=>{
     let info=JSON.parse(Object.keys(req.body)[0]);
-    let client=new MongoClient("mongodb+srv://apo:jac2001min@cluster0.pdunp.mongodb.net/?retryWrites=true&w=majority")
+    let client=new MongoClient("mongodb://apo:jac2001min@cluster0-shard-00-00.pdunp.mongodb.net:27017,cluster0-shard-00-01.pdunp.mongodb.net:27017,cluster0-shard-00-02.pdunp.mongodb.net:27017/?ssl=true&replicaSet=atlas-me2tz8-shard-0&authSource=admin&retryWrites=true&w=majority")
     client.db("ristoro").collection("ristoranti").updateOne({_id:new ObjectId(info.id),"carrello.id":new ObjectId(info.idCarrello)},{$set:{"carrello.$.quantita":info.quantita}},(err,result)=>{
         if (err) throw err;
     })
@@ -58,7 +58,8 @@ app.put("/modify-carrello", async (req,res)=>{
 //cancella elemento dal carrello
 app.put("/delate-cart", async (req,res)=>{
     let info=JSON.parse(Object.keys(req.body)[0]);
-    let client=new MongoClient("mongodb+srv://apo:jac2001min@cluster0.pdunp.mongodb.net/?retryWrites=true&w=majority")
+    console.log(info);
+    let client=new MongoClient("mongodb://apo:jac2001min@cluster0-shard-00-00.pdunp.mongodb.net:27017,cluster0-shard-00-01.pdunp.mongodb.net:27017,cluster0-shard-00-02.pdunp.mongodb.net:27017/?ssl=true&replicaSet=atlas-me2tz8-shard-0&authSource=admin&retryWrites=true&w=majority")
     client.db("ristoro").collection("ristoranti").updateOne({_id:new ObjectId(info.id)},{$pull:{carrello:{id:new ObjectId(info.idCarrello)}}},(err,result)=>{
         if (err) throw err;
     })
@@ -66,7 +67,7 @@ app.put("/delate-cart", async (req,res)=>{
 //add consegna
 app.put("/add-consegna", async (req,res)=>{
     let info=JSON.parse(Object.keys(req.body)[0]);
-    let client=new MongoClient("mongodb+srv://apo:jac2001min@cluster0.pdunp.mongodb.net/?retryWrites=true&w=majority")
+    let client=new MongoClient("mongodb://apo:jac2001min@cluster0-shard-00-00.pdunp.mongodb.net:27017,cluster0-shard-00-01.pdunp.mongodb.net:27017,cluster0-shard-00-02.pdunp.mongodb.net:27017/?ssl=true&replicaSet=atlas-me2tz8-shard-0&authSource=admin&retryWrites=true&w=majority")
     client.db("ristoro").collection("ristoranti").updateOne({_id:new ObjectId(info.id)},{$push:{consegna:{id:new ObjectId(),ora:info.ora,via:info.via,citta:info.citta,prezzo:info.prezzo,stato:0,cliente:info.cliente}}},(err,result)=>{
         if (err) throw err;
     })
@@ -74,7 +75,7 @@ app.put("/add-consegna", async (req,res)=>{
 //modify consegna
 app.put("/modify-consegna", async (req,res)=>{
     let info=JSON.parse(Object.keys(req.body)[0]);
-    let client=new MongoClient("mongodb+srv://apo:jac2001min@cluster0.pdunp.mongodb.net/?retryWrites=true&w=majority")
+    let client=new MongoClient("mongodb://apo:jac2001min@cluster0-shard-00-00.pdunp.mongodb.net:27017,cluster0-shard-00-01.pdunp.mongodb.net:27017,cluster0-shard-00-02.pdunp.mongodb.net:27017/?ssl=true&replicaSet=atlas-me2tz8-shard-0&authSource=admin&retryWrites=true&w=majority")
     client.db("ristoro").collection("ristoranti").updateOne({_id:new ObjectId(info.id),"consegna.id":new ObjectId(info.idConsegna)},{$set:{"consegna.$.stato":info.stato}},(err,result)=>{
         if (err) throw err;
     })
@@ -82,7 +83,7 @@ app.put("/modify-consegna", async (req,res)=>{
 //cancella elemento da consegna
 app.put("/delate-consegna", async (req,res)=>{
     let info=JSON.parse(Object.keys(req.body)[0]);
-    let client=new MongoClient("mongodb+srv://apo:jac2001min@cluster0.pdunp.mongodb.net/?retryWrites=true&w=majority")
+    let client=new MongoClient("mongodb://apo:jac2001min@cluster0-shard-00-00.pdunp.mongodb.net:27017,cluster0-shard-00-01.pdunp.mongodb.net:27017,cluster0-shard-00-02.pdunp.mongodb.net:27017/?ssl=true&replicaSet=atlas-me2tz8-shard-0&authSource=admin&retryWrites=true&w=majority")
     client.db("ristoro").collection("ristoranti").updateOne({_id:new ObjectId(info.id)},{$pull:{consegna:{id:new ObjectId(info.idConsegna)}}},(err,result)=>{
         if (err) throw err;
     })
