32084b3fe87b15229b212717307a3d06b3a86205
jacopomino <92917960+jacopomino@users.noreply.github.com>
2023-09-21 17:26:35 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import cors from "cors"
 import express from "express"
 import bodyParser from "body-parser"
 import {MongoClient,ObjectId} from "mongodb"
+import { intersectRanges } from "@fullcalendar/core/internal";
 
 const PORT = process.env.PORT|| 3001;
 const app=express()
@@ -10,17 +11,100 @@ app.use(bodyParser.urlencoded({extended:true}))
 app.listen(PORT,()=>{
     console.log("run");
 })
-//get ristoranti
-app.get("/ristoranti", async (req,res)=>{
-    let client=new MongoClient("mongodb://apo:jac2001min@cluster0-shard-00-00.pdunp.mongodb.net:27017,cluster0-shard-00-01.pdunp.mongodb.net:27017,cluster0-shard-00-02.pdunp.mongodb.net:27017/?ssl=true&replicaSet=atlas-me2tz8-shard-0&authSource=admin&retryWrites=true&w=majority")
-    client.db("ristoro").collection("ristoranti").find({}).toArray().then(e=>res.send(e))
-})
-//put signup
+//signup attivita
 app.put("/signup", async (req,res)=>{
     let info=JSON.parse(Object.keys(req.body)[0]);
     let client=new MongoClient("mongodb://apo:jac2001min@cluster0-shard-00-00.pdunp.mongodb.net:27017,cluster0-shard-00-01.pdunp.mongodb.net:27017,cluster0-shard-00-02.pdunp.mongodb.net:27017/?ssl=true&replicaSet=atlas-me2tz8-shard-0&authSource=admin&retryWrites=true&w=majority")
-    client.db("ristoro").collection("ristoranti").insertOne(info,(err,result)=>{
-        if (err) throw err;
+    client.db("ristoro").collection("ristoranti").findOne({nomeAttivita:info.nomeAttivita}).then(e=>{
+        if(!e){
+            client.db("ristoro").collection("ristoranti").insertOne(info).then(i=>{
+                if(!i){
+                    res.status(203).send("Non è avvenuto corretamente il procedimento")
+                }else{
+                    res.send(i.insertedId)
+                }
+            })
+        }else{
+            res.status(203).send("Attività già registrata")
+        }
+    })
+})
+//login attivita
+app.put("/login", async (req,res)=>{
+    let info=JSON.parse(Object.keys(req.body)[0]);
+    let client=new MongoClient("mongodb://apo:jac2001min@cluster0-shard-00-00.pdunp.mongodb.net:27017,cluster0-shard-00-01.pdunp.mongodb.net:27017,cluster0-shard-00-02.pdunp.mongodb.net:27017/?ssl=true&replicaSet=atlas-me2tz8-shard-0&authSource=admin&retryWrites=true&w=majority")
+    client.db("ristoro").collection("ristoranti").findOne({password:info.password}).then(e=>{
+        if(e){
+            res.send(e._id)
+        }else{
+            res.status(203).send("Attività non esistente, Registrati!")
+        }
+    })
+})
+//login dipendente
+app.put("/dipLogin", async (req,res)=>{
+    let info=JSON.parse(Object.keys(req.body)[0]);
+    let client=new MongoClient("mongodb://apo:jac2001min@cluster0-shard-00-00.pdunp.mongodb.net:27017,cluster0-shard-00-01.pdunp.mongodb.net:27017,cluster0-shard-00-02.pdunp.mongodb.net:27017/?ssl=true&replicaSet=atlas-me2tz8-shard-0&authSource=admin&retryWrites=true&w=majority")
+    client.db("ristoro").collection("ristoranti").findOne({nomeAttivita:info.na}).then(e=>{
+        if(!e){
+            res.status(203).send("Attività non esistente")
+        }else{
+            client.db("ristoro").collection("fattorino").findOne({nome:info.nome}).then(t=>{
+                if(!t){
+                    const id=new ObjectId()
+                    client.db("ristoro").collection("ristoranti").updateOne({_id:new ObjectId(e._id)},{$push:{fattorino:{id:id}}}).then(i=>{
+                        if(!i){
+                            res.status(203).send("Non è avvenuto corretamente il procedimento")
+                        }else{
+                            client.db("ristoro").collection("fattorino").insertOne({_id:id,idAttivta:e._id,nome:info.nome}).then(k=>{
+                                if(!k){
+                                    res.status(203).send("Non è avvenuto corretamente il procedimento")
+                                }else{
+                                    res.send(id)
+                                }
+                            })
+                        }
+                    })
+                    
+                }else{
+                    res.send(t._id);
+                }
+            })
+        }
+    })
+})
+//stai loggato
+app.put("/stayLoggedIn", async (req,res)=>{
+    let info=JSON.parse(Object.keys(req.body)[0]);
+    let client=new MongoClient("mongodb://apo:jac2001min@cluster0-shard-00-00.pdunp.mongodb.net:27017,cluster0-shard-00-01.pdunp.mongodb.net:27017,cluster0-shard-00-02.pdunp.mongodb.net:27017/?ssl=true&replicaSet=atlas-me2tz8-shard-0&authSource=admin&retryWrites=true&w=majority")
+    client.db("ristoro").collection("ristoranti").findOne({_id:new ObjectId(info._id)}).then(i=>{
+        if(!i){
+            res.status(203).send("Token non valido")
+        }else{
+            client.db("ristoro").collection("fattorino").find({idAttivta:new ObjectId(i._id)}).toArray().then(e=>{
+                let obj={attivita:i,fattorino:e}
+                res.status(200).send(obj)
+            })
+        }
+    })
+})
+//stai loggato dipendente
+app.put("/dipStayLoggedIn", async (req,res)=>{
+    let info=JSON.parse(Object.keys(req.body)[0]);
+    let client=new MongoClient("mongodb://apo:jac2001min@cluster0-shard-00-00.pdunp.mongodb.net:27017,cluster0-shard-00-01.pdunp.mongodb.net:27017,cluster0-shard-00-02.pdunp.mongodb.net:27017/?ssl=true&replicaSet=atlas-me2tz8-shard-0&authSource=admin&retryWrites=true&w=majority")
+    client.db("ristoro").collection("fattorino").findOne({_id:new ObjectId(info._id)}).then(e=>{
+        if(!e){
+            res.status(203).send("Token non valido")
+        }else{
+            client.db("ristoro").collection("ristoranti").findOne({"fattorino.id":new ObjectId(info._id)}).then(i=>{
+                if(!i){
+                    res.status(203).send("Token non valido")
+                }else{
+                    let obj={attivita:i,fattorino:e}
+                    res.status(200).send(obj)
+                }
+            }) 
+        }
     })
 })
 //add elemento in dispensa
@@ -66,25 +150,58 @@ app.put("/delate-cart", async (req,res)=>{
 //add consegna
 app.put("/add-consegna", async (req,res)=>{
     let info=JSON.parse(Object.keys(req.body)[0]);
-    let client=new MongoClient("mongodb://apo:jac2001min@cluster0-shard-00-00.pdunp.mongodb.net:27017,cluster0-shard-00-01.pdunp.mongodb.net:27017,cluster0-shard-00-02.pdunp.mongodb.net:27017/?ssl=true&replicaSet=atlas-me2tz8-shard-0&authSource=admin&retryWrites=true&w=majority")
-    client.db("ristoro").collection("ristoranti").updateOne({_id:new ObjectId(info.id)},{$push:{consegna:{id:new ObjectId(),ora:info.ora,via:info.via,citta:info.citta,prezzo:info.prezzo,stato:0,cliente:info.cliente}}},(err,result)=>{
-        if (err) throw err;
-    })
+    if(info.via!==""&&info.citta!==""&&info.citofono!==""&&info.prezzo!==""&&info.ora!==""&&info.ordine!==""){
+        let client=new MongoClient("mongodb://apo:jac2001min@cluster0-shard-00-00.pdunp.mongodb.net:27017,cluster0-shard-00-01.pdunp.mongodb.net:27017,cluster0-shard-00-02.pdunp.mongodb.net:27017/?ssl=true&replicaSet=atlas-me2tz8-shard-0&authSource=admin&retryWrites=true&w=majority")
+        client.db("ristoro").collection("ristoranti").updateOne({_id:new ObjectId(info.id)},{$push:{consegna:{id:new ObjectId(),ora:info.ora,data:info.data,via:info.via,citta:info.citta,prezzo:parseFloat(info.prezzo),stato:0,citofono:info.citofono,ordine:info.ordine}}}).then(e=>{
+            if(!e){
+                res.status(203).send("Non è avvenuto corretamente il procedimento")
+            }else{
+                res.send("Ok")
+            }
+        })
+    }else{
+        res.status(203).send("Attenzione! Compila tutti i campi")
+    }
 })
 //modify consegna
 app.put("/modify-consegna", async (req,res)=>{
     let info=JSON.parse(Object.keys(req.body)[0]);
     let client=new MongoClient("mongodb://apo:jac2001min@cluster0-shard-00-00.pdunp.mongodb.net:27017,cluster0-shard-00-01.pdunp.mongodb.net:27017,cluster0-shard-00-02.pdunp.mongodb.net:27017/?ssl=true&replicaSet=atlas-me2tz8-shard-0&authSource=admin&retryWrites=true&w=majority")
-    client.db("ristoro").collection("ristoranti").updateOne({_id:new ObjectId(info.id),"consegna.id":new ObjectId(info.idConsegna)},{$set:{"consegna.$.stato":info.stato}},(err,result)=>{
-        if (err) throw err;
+    client.db("ristoro").collection("ristoranti").updateOne({_id:new ObjectId(info.id),"consegna.id":new ObjectId(info.idConsegna)},{$set:{"consegna.$.stato":info.stato,"consegna.$.fattorino":new ObjectId(info.fattorino)}}).then(e=>{
+        if(!e){
+            res.status(203).send("Non è avvenuto corretamente il procedimento")
+        }else{
+            if(info.stato===2){
+                client.db("ristoro").collection("fattorino").updateOne({_id:new ObjectId(info.fattorino)},{$push:{consegna:{id:new ObjectId(info.idConsegna),stato:info.stato}}}).then((i=>{
+                    if(!i){
+                        res.status(203).send("Non è avvenuto corretamente il procedimento")
+                    }else{
+                        res.send("Ok")
+                    }
+                }))
+            }else{
+                client.db("ristoro").collection("fattorino").updateOne({_id:new ObjectId(info.fattorino),"consegna.id":new ObjectId(info.idConsegna)},{$set:{"consegna.$.stato":info.stato,"consegna.$.fattorino":info.fattorino}}).then((i=>{
+                    if(!i){
+                        res.status(203).send("Non è avvenuto corretamente il procedimento")
+                    }else{
+                        res.send("Ok")
+                    }
+                }))
+            }
+            
+        }
     })
 })
 //cancella elemento da consegna
 app.put("/delate-consegna", async (req,res)=>{
     let info=JSON.parse(Object.keys(req.body)[0]);
     let client=new MongoClient("mongodb://apo:jac2001min@cluster0-shard-00-00.pdunp.mongodb.net:27017,cluster0-shard-00-01.pdunp.mongodb.net:27017,cluster0-shard-00-02.pdunp.mongodb.net:27017/?ssl=true&replicaSet=atlas-me2tz8-shard-0&authSource=admin&retryWrites=true&w=majority")
-    client.db("ristoro").collection("ristoranti").updateOne({_id:new ObjectId(info.id)},{$pull:{consegna:{id:new ObjectId(info.idConsegna)}}},(err,result)=>{
-        if (err) throw err;
+    client.db("ristoro").collection("ristoranti").updateOne({_id:new ObjectId(info.id)},{$pull:{consegna:{id:new ObjectId(info.idConsegna)}}}).then(e=>{
+        if(!e){
+            res.status(203).send("Non è avvenuto corretamente il procedimento")
+        }else{
+            res.send("Ok")
+        }
     })
 })
 //add prenotazione
