7ddda0adc425dd8c3cef6906e5b542e1d273be4a
jacopomino <92917960+jacopomino@users.noreply.github.com>
2023-10-02 11:40:17 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -14,69 +14,155 @@ const client=new MongoClient("mongodb://apo:jac2001min@cluster0-shard-00-00.pdun
 //signup attivita
 app.put("/signup", async (req,res)=>{
     let info=JSON.parse(Object.keys(req.body)[0]);
+    let countError=0
+    let error="non hai compilato il campo: "
+    if(info.nomeAttivita===""){
+        countError++
+        error=error+"nome attività, "
+    }
+    if(info.password===""){
+        countError++
+        error=error+"password, "
+    }
+    if(info.email===""){
+        countError++
+        error=error+"email, "
+    }
+    if(info.numeroTelefono===""){
+        countError++
+        error=error+"numero di telefono, "
+    }
+    if(info.indirizzo===""){
+        countError++
+        error=error+"indirizzo della attività, "
+    }
+    if(countError>0){
+        res.status(203).send(error)
+    }else{
+        client.db("ristoro").collection("ristoranti").findOne({nomeAttivita:info.nomeAttivita}).then(e=>{
+            if(!e){
+                client.db("ristoro").collection("ristoranti").insertOne(info).then(i=>{
+                    if(!i){
+                        res.status(203).send("Non è avvenuto corretamente il procedimento")
+                    }else{
+                        res.send(i.insertedId)
+                    }
+                })
+            }else{
+                res.status(203).send("Attività già registrata")
+            }
+        })
+    }
     
-    client.db("ristoro").collection("ristoranti").findOne({nomeAttivita:info.nomeAttivita}).then(e=>{
-        if(!e){
-            client.db("ristoro").collection("ristoranti").insertOne(info).then(i=>{
-                if(!i){
-                    res.status(203).send("Non è avvenuto corretamente il procedimento")
-                }else{
-                    res.send(i.insertedId)
-                }
-            })
-        }else{
-            res.status(203).send("Attività già registrata")
-        }
-    })
 })
 //login attivita
 app.put("/login", async (req,res)=>{
     let info=JSON.parse(Object.keys(req.body)[0]);
+    let countError=0
+    let error="non hai compilato il campo: "
+    if(info.password===""){
+        countError++
+        error=error+"password, "
+    }
+    if(info.email===""){
+        countError++
+        error=error+"email, "
+    }
+    if(countError>0){
+        res.status(203).send(error)
+    }else{
+        client.db("ristoro").collection("ristoranti").findOne({password:info.password,email:info.email}).then(e=>{
+            if(e){
+                res.send(e._id)
+            }else{
+                res.status(203).send("Attività non esistente, Registrati!")
+            }
+        })
+    }
     
-    client.db("ristoro").collection("ristoranti").findOne({password:info.password}).then(e=>{
-        if(e){
-            res.send(e._id)
-        }else{
-            res.status(203).send("Attività non esistente, Registrati!")
-        }
-    })
+})
+//signup dipendente
+app.put("/dipSignup", async (req,res)=>{
+    let info=JSON.parse(Object.keys(req.body)[0]);
+    let countError=0
+    let error="non hai compilato il campo: "
+    if(info.nome===""){
+        countError++
+        error=error+"nome, "
+    }
+    if(info.nomeAttivita===""){
+        countError++
+        error=error+"nome attività, "
+    }
+    if(info.email===""){
+        countError++
+        error=error+"email, "
+    }
+    if(countError>0){
+        res.status(203).send(error)
+    }else{
+        client.db("ristoro").collection("ristoranti").findOne({nomeAttivita:info.nomeAttivita}).then(e=>{
+            if(!e){
+                res.status(203).send("Attività non esistente")
+            }else{
+                client.db("ristoro").collection("fattorino").findOne({nome:info.nome,idAttivita:e._id}).then(t=>{
+                    if(!t){
+                        const id=new ObjectId()
+                        client.db("ristoro").collection("ristoranti").updateOne({_id:new ObjectId(e._id)},{$push:{fattorino:{id:id}}}).then(i=>{
+                            if(!i){
+                                res.status(203).send("Non è avvenuto corretamente il procedimento")
+                            }else{
+                                client.db("ristoro").collection("fattorino").insertOne({_id:id,idAttivita:e._id,nome:info.nome,email:info.email,dataDiRegistrazione:info.dataDiRegistrazione}).then(k=>{
+                                    if(!k){
+                                        res.status(203).send("Non è avvenuto corretamente il procedimento")
+                                    }else{
+                                        res.send(id)
+                                    }
+                                })
+                            }
+                        })
+                    }else{
+                        res.status(203).send("Dipendente già esistente")
+                    }
+                })
+            }
+        })
+    }
 })
 //login dipendente
 app.put("/dipLogin", async (req,res)=>{
     let info=JSON.parse(Object.keys(req.body)[0]);
-    
-    client.db("ristoro").collection("ristoranti").findOne({nomeAttivita:info.na}).then(e=>{
-        if(!e){
-            res.status(203).send("Attività non esistente")
-        }else{
-            client.db("ristoro").collection("fattorino").findOne({nome:info.nome}).then(t=>{
-                if(!t){
-                    const id=new ObjectId()
-                    client.db("ristoro").collection("ristoranti").updateOne({_id:new ObjectId(e._id)},{$push:{fattorino:{id:id}}}).then(i=>{
-                        if(!i){
-                            res.status(203).send("Non è avvenuto corretamente il procedimento")
-                        }else{
-                            client.db("ristoro").collection("fattorino").insertOne({_id:id,idAttivta:e._id,nome:info.nome}).then(k=>{
-                                if(!k){
-                                    res.status(203).send("Non è avvenuto corretamente il procedimento")
-                                }else{
-                                    res.send(id)
-                                }
-                            })
-                        }
-                    })
-                    
-                }else{
-                    res.send(t._id);
-                }
-            })
-        }
-    })
+    let countError=0
+    let error="non hai compilato il campo: "
+    if(info.nome===""){
+        countError++
+        error=error+"nome, "
+    }
+    if(info.nomeAttivita===""){
+        countError++
+        error=error+"nome attivita, "
+    }
+    if(countError>0){
+        res.status(203).send(error)
+    }else{
+        client.db("ristoro").collection("ristoranti").findOne({nomeAttivita:info.nomeAttivita}).then(e=>{
+            if(!e){
+                res.status(203).send("Attività non esistente")
+            }else{
+                client.db("ristoro").collection("fattorino").findOne({nome:info.nome,idAttivita:e._id}).then(t=>{
+                    if(!t){
+                        res.status(203).send("Dipendente non esistente")
+                    }else{
+                        res.send(t._id);
+                    }
+                })
+            }
+        })
+    }
 })
 //stai loggato
 app.put("/stayLoggedIn", async (req,res)=>{
     let info=JSON.parse(Object.keys(req.body)[0]);
-    
     client.db("ristoro").collection("ristoranti").findOne({_id:new ObjectId(info._id)}).then(i=>{
         if(!i){
             res.status(203).send("Token non valido")
@@ -91,7 +177,6 @@ app.put("/stayLoggedIn", async (req,res)=>{
 //stai loggato dipendente
 app.put("/dipStayLoggedIn", async (req,res)=>{
     let info=JSON.parse(Object.keys(req.body)[0]);
-    
     client.db("ristoro").collection("fattorino").findOne({_id:new ObjectId(info._id)}).then(e=>{
         if(!e){
             res.status(203).send("Token non valido")
@@ -110,7 +195,6 @@ app.put("/dipStayLoggedIn", async (req,res)=>{
 //add elemento in dispensa
 app.put("/add-dispensa", async (req,res)=>{
     let info=JSON.parse(Object.keys(req.body)[0]);
-    
     client.db("ristoro").collection("ristoranti").updateOne({_id:new ObjectId(info.id)},{$push:{"dispensa":{id:new ObjectId(),elemento:info.elemento,categoria:info.categoria}}},(err,result)=>{
         if (err) throw err;
     })
@@ -118,7 +202,6 @@ app.put("/add-dispensa", async (req,res)=>{
 //modify elemento in dispensa
 app.put("/modify-dispensa", async (req,res)=>{
     let info=JSON.parse(Object.keys(req.body)[0]);
-    
     client.db("ristoro").collection("ristoranti").updateOne({_id:new ObjectId(info.id),"dispensa.id":new ObjectId(info.idDispensa)},{$set:{"dispensa.$.quantita":info.quantita}},(err,result)=>{
         if (err) throw err;
     })
@@ -126,7 +209,6 @@ app.put("/modify-dispensa", async (req,res)=>{
 //add cart
 app.put("/add-cart", async (req,res)=>{
     let info=JSON.parse(Object.keys(req.body)[0]);
-    
     client.db("ristoro").collection("ristoranti").updateOne({_id:new ObjectId(info.id)},{$push:{carrello:{id:new ObjectId(info.idCarrello),elemento:info.elemento}}},(err,result)=>{
         if (err) throw err;
     })
@@ -134,7 +216,6 @@ app.put("/add-cart", async (req,res)=>{
 //modify elemento in cart
 app.put("/modify-carrello", async (req,res)=>{
     let info=JSON.parse(Object.keys(req.body)[0]);
-    
     client.db("ristoro").collection("ristoranti").updateOne({_id:new ObjectId(info.id),"carrello.id":new ObjectId(info.idCarrello)},{$set:{"carrello.$.quantita":info.quantita}},(err,result)=>{
         if (err) throw err;
     })
@@ -186,7 +267,6 @@ app.put("/modify-consegna", async (req,res)=>{
                     }
                 }))
             }
-            
         }
     })
 })
@@ -204,7 +284,6 @@ app.put("/delate-consegna", async (req,res)=>{
 //add prenotazione
 app.put("/add-prenotazione", async (req,res)=>{
     let info=JSON.parse(Object.keys(req.body)[0]);
-    
     client.db("ristoro").collection("ristoranti").updateOne({_id:new ObjectId(info.id)},{$push:{prenotazione:{id:new ObjectId(),title:info.text,date:info.dataOra}}},(err,result)=>{
         if (err) throw err;
     })
@@ -212,7 +291,6 @@ app.put("/add-prenotazione", async (req,res)=>{
 //cancella prenotazione
 app.put("/delate-prenotazione", async (req,res)=>{
     let info=JSON.parse(Object.keys(req.body)[0]);
-    
     client.db("ristoro").collection("ristoranti").updateOne({_id:new ObjectId(info.id)},{$pull:{prenotazione:{id:new ObjectId(info.idPrenotazione)}}},(err,result)=>{
         if (err) throw err;
     })
